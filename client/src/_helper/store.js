a362b5cbca2f18d481d62ab382cdfefa490af333
tcrepy <tanguycrepy@gmail.com>
2019-07-16 19:46:40 +0200

    context-login: refactor context, logout, affichage selon state user

--- client/src/_helper/store.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, createStore} from "redux";
-import rootReducer from "../redux/reducer/RootReducer";
-import thunkMiddleware from "redux-thunk";
-
-export const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware
-    ));
