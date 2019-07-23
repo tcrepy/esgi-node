0bfa399028a9589319d9f2b1d4241c0b729ff309
waxime <aveline.maxime@gmail.com>
2019-07-23 23:41:24 +0200

    homepage: Remove useless imports

--- client/src/Components/Nav/Homepage.js
+++ client/src/Components/Nav/Homepage.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
 import {LinkConstants} from '../../_constants/link.constants';
 import {history} from "../../_helper/history";
-import Icon from '@material-ui/core/Icon';
-import { Copyright } from '@material-ui/icons';
 
 function CopyrightText() {
   return (
